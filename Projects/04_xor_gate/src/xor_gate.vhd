228d6d08629197a5a6995dfb1d5b6a79814f93f3
murugan-di <122307551+murugan-di@users.noreply.github.com>
2026-03-25 10:39:14 -0400

    Add files via upload

---
+++ Projects/04_xor_gate/src/xor_gate.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/25/2026 10:28:24 AM
+-- Design Name: 
+-- Module Name: xor_gate - rtl
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity xor_gate is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           out1 : out STD_LOGIC);
+end xor_gate;
+
+architecture rtl of xor_gate is
+
+begin
+    out1 <= a xor b;
+
+end rtl;
